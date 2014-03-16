8e3dabc1a3ed2f346b379c169a7abf7d96a26c9e
Oleksii Mylotskyi <spalaxinco@gmail.com>
2014-03-16 21:18:43 +0200

    Added global Locale entity, it is will represent supported locales.

---
+++ src/HcCore/Entity/Locale.php
@@ -0,0 +1,36 @@
+<?php
+namespace HcCore\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Locale
+ *
+ * @ORM\Table(name="locale")
+ * @ORM\Entity
+ */
+class Locale implements EntityInterface
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="locale", type="string", length=5, nullable=false)
+     */
+    private $locale;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="lang", type="string", length=2, nullable=false)
+     */
+    private $lang;
+}
