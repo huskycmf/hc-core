71062d64977cd325afcec9241b299471c5b61e1b
Oleksii Mylotskyi <spalaxinco@gmail.com>
2014-04-17 15:25:04 +0300

    Added throws exception if resource has invalid type

--- src/HcCore/Service/Fetch/Paginator/QueryBuilder/ResourceDataServiceInterface.php
+++ src/HcCore/Service/Fetch/Paginator/QueryBuilder/ResourceDataServiceInterface.php
@@ -3,6 +3,7 @@ namespace HcCore\Service\Fetch\Paginator\QueryBuilder;
 
 use Doctrine\ORM\QueryBuilder;
 use HcCore\Entity\EntityInterface;
+use HcCore\Service\Fetch\Paginator\Exception\InvalidResourceException;
 use HcCore\Service\Fetch\Paginator\ResourceDataInterface;
 use Zend\Stdlib\Parameters;
 
@@ -11,6 +12,7 @@ interface ResourceDataServiceInterface extends ResourceDataInterface
     /**
      * @param mixed | EntityInterface $resource
      * @param Parameters $params [OPTIONAL]
+     * @throws InvalidResourceException
      * @return QueryBuilder
      */
     public function fetch($resource, Parameters $params = null);
